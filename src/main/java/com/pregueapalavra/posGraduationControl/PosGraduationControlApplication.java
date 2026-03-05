1b7c2f0fb3957f5c80ff8e55bcf5030458609273
Alexandre Matos <matosalexandre795@gmail.com>
2026-03-05 20:46:31 -0300

    Projeto Criado!

---
+++ src/main/java/com/pregueapalavra/posGraduationControl/PosGraduationControlApplication.java
@@ -0,0 +1,13 @@
+package com.pregueapalavra.posGraduationControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosGraduationControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosGraduationControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pregueapalavra/posGraduationControl/PosGraduationControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pregueapalavra.posGraduationControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosGraduationControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
